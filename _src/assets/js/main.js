3b4ebd4cb97f8d306dde343d11f6e6e139658b91
tanialopezab <tania.lopez.ab@gmail.com>
2019-04-27 18:07:11 +0200

    resolve first part of exercice

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('>> Ready :)');
+const inputNameElement = document.getElementById('insertName');
+const inputEmailElement = document.getElementById('insertEmail');
+const checkVerifyElement = document.getElementById('checkVerify');
+const saveButtonElement = document.querySelector('.button__save');
+const userInfoArr = [];
+
+
+
+function getInforFromForm() {
+  const infoUser = {};// está aqui porque la variable que contiene ese objeto NO PUEDE SER GLOBAL Se crea una nueva y se destruye cada vez que se ejecute la función.
+  if (inputNameElement.value !== '') {
+    infoUser.name = inputNameElement.value;
+    infoUser.confirm = checkVerifyElement.checked;
+    infoUser.email = inputEmailElement.value;
+
+    userInfoArr.push(infoUser);
+
+    console.log(userInfoArr);
+
+  }
+}
+
+
+saveButtonElement.addEventListener('click', getInforFromForm);
+
